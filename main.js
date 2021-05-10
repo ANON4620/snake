e31cb6034b0c693bab3f7ed5246a4c873788a985
ANON4620 <swarnadeep.click@gmail.com>
2021-05-11 00:16:43 +0530

    Added Food, Bug fixes and performance improvements

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 const rightBtn = document.getElementById('right');
 const leftBtn = document.getElementById('left');
 const upBtn = document.getElementById('up');
 const downBtn = document.getElementById('down');
 
+const intervalPerFrame = 300;
+let state = 'START';
 let key;
+
 
 function removeAllEventListeners() {
   rightBtn.removeEventListener('click', moveR);
@@ -16,105 +19,113 @@ function removeAllEventListeners() {
 }
 
 class Snake {
-  constructor() {
-    this.height = 10;
-    this.width = 10;
-    this.x = 0;
-    this.y = 0;
+  constructor(x, y, width, height) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.differenceBetweenEachMove = 10;
   }
   
-  moveR() {
-    key = 'Right';
+  draw() {
     const move = setInterval(() => {
-      ctx.fillRect(this.x, this.y, snake.width, snake.height);
-      ctx.fillRect(this.x + 10, this.y, snake.width, snake.height);
-      ctx.clearRect(this.x - 10, this.y, snake.width, snake.height);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
       
-      this.x += 10;
+      food.draw();
+      ctx.fillStyle = 'Black';
+      ctx.fillRect(this.x, this.y, this.width, this.height);
       
-      if(this.x >= canvas.width) {
+      switch(key) {
+        case 'RIGHT':
+          this.x += this.differenceBetweenEachMove;
+          break;
+          
+        case 'LEFT':
+          this.x -= this.differenceBetweenEachMove;
+          break;
+          
+        case 'UP':
+          this.y -= this.differenceBetweenEachMove;
+          break;
+          
+        case 'DOWN':
+          this.y += this.differenceBetweenEachMove;
+          break;
+      }
+      
+      if(food.hasEaten())
+        food.changePosition();
+      
+      if(this.x >= canvas.width + 10 ||
+         this.y >= canvas.height + 10 ||
+         this.x <= -20 ||
+         this.y <= -20) {
         clearInterval(move);
         alert('Game Over!');
         removeAllEventListeners();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
       }
-      if(key !== 'Right') {
-        clearInterval(move);
-        ctx.clearRect(this.x - 10, this.y, snake.width, snake.height);
-      }
-    }, 100);
-  }
-  
-  moveL() {
-    key = 'Left';
-    const move = setInterval(() => {
-      ctx.fillRect(this.x, this.y, snake.width, snake.height);
-      ctx.fillRect(this.x - 10, this.y, snake.width, snake.height);
-      ctx.clearRect(this.x + 10, this.y, snake.width, snake.height);
-    
-      this.x -= 10;
-      
-      if(this.x <= -10) {
-        clearInterval(move);
-        alert('Game Over!');
-        removeAllEventListeners();
-      }
-      if(key !== 'Left') {
-        clearInterval(move);
-        ctx.clearRect(this.x + 10, this.y, snake.width, snake.height);
-      }
-    }, 100);
-  }
-  
-  moveU() {
-    key = 'Up';
-    const move = setInterval(() => {
-      ctx.fillRect(this.x, this.y, snake.width, snake.height);
-      ctx.fillRect(this.x, this.y - 10, snake.width, snake.height);
-      ctx.clearRect(this.x, this.y + 10, snake.width, snake.height);
-    
-      this.y -= 10;
-      
-      if(this.y <= -10) {
-        clearInterval(move);
-        alert('Game Over!');
-        removeAllEventListeners();
-      }
-      if(key !== 'Up') {
-        clearInterval(move);
-        ctx.clearRect(this.x, this.y + 10, snake.width, snake.height);
-      }
-    }, 100);
-  }
-  
-  moveD() {
-    key = 'Down';
-    const move = setInterval(() => {
-      ctx.fillRect(this.x, this.y, snake.width, snake.height);
-      ctx.fillRect(this.x, this.y + 10, snake.width, snake.height);
-      ctx.clearRect(this.x, this.y - 10, snake.width, snake.height);
-      
-      this.y += 10;
-      
-      if(this.y >= canvas.height) {
-        clearInterval(move);
-        alert('Game Over!');
-        removeAllEventListeners();
-      }
-      if(key !== 'Down') {
-        clearInterval(move);
-        ctx.clearRect(this.x, this.y - 10, snake.width, snake.height);
-      }
-    }, 100);
+    }, intervalPerFrame);
   }
 }
 
+class Food {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.x;
+    this.y;
+    this.changePosition();
+  }
+  
+  draw() {
+    ctx.fillStyle = 'Red';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  
+  changePosition() {
+    this.x = parseInt(((Math.random() * 200) + 1) / 10) * 10;
+    this.y = parseInt(((Math.random() * 200) + 1) / 10) * 10;
+  }
+  
+  hasEaten() {
+    if(snake.x === food.x && snake.y === food.y) {
+      return true;
+    }
+  }
+}
 
-const snake = new Snake();
+const snake = new Snake(0, 0, 10, 10);
+const food = new Food(10, 10);
 
-function moveR() {snake.moveR()}
-function moveL() {snake.moveL()}
-function moveU() {snake.moveU()}
-function moveD() {snake.moveD()}
+function moveR() {
+  if(key !== 'LEFT' && key !== 'RIGHT') {
+    key = 'RIGHT';
+    if(state === 'START') {
+      state = 'STOP';
+      snake.draw();
+    }
+  }
+}
+function moveL() {
+  if(key !== 'LEFT' && key !== 'RIGHT') {
+    key = 'LEFT';
+  }
+}
+function moveU() {
+  if(key !== 'UP' && key !== 'DOWN') {
+    key = 'UP';
+  }
+}
+function moveD() {
+  if(key !== 'UP' && key !== 'DOWN') {
+    key = 'DOWN';
+    if(state === 'START') {
+      state = 'STOP';
+      snake.draw();
+    }
+  }
+}
 
 rightBtn.addEventListener('click', moveR);
 leftBtn.addEventListener('click', moveL);
